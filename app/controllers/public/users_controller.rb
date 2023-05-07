f6f60617f8d09ce1080c18623838b4251d0131e5
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-05-07 13:16:13 +0000

    create_user詳細_編集

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,9 +1,31 @@
 class Public::UsersController < ApplicationController
 
   def show
+    @user = current_user
+    @posts = @user.posts
   end
 
   def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      flash[:notice] = "編集しました"
+      redirect_to user_path(@user)
+    else
+      flash[:alert] = "空欄を入力してください"
+      redirect_to edit_post_path(@post)
+    end
+  end
+
+
+  # 投稿データのストロングパラメータ
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :image, :introduction, :email, :encrypted_password, :members_status)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,14 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
 
+  has_one_attached :profile_image
+
+  def get_profile_image(width,height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/noimage.png')
+      profile_image.attach(io: File.open(file_path),filename: 'default-image.png',content_type: 'image/png')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+
 end
