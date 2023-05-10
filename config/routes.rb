e8cea00669dfab8b93aa4870c15b2b84be23636b
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-05-10 07:31:43 +0000

    create_いいね一覧

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
+
   # 会員ゲストログイン
   devise_scope :user do
     post '/users/guest_sign_in' => 'public/sessions#guest_sign_in'
@@ -21,11 +21,11 @@ Rails.application.routes.draw do
     resources :posts, only: [:index, :show, :edit, :update, :destroy] do
       resources :comments, only: [:create, :destroy]
     end
-    
+
     resources :users, only: [:index, :show, :edit, :update]
-    get "search" => "searches#search" 
+    get "search" => "searches#search"
   end
-  
+
 
   # ユーザー側
   scope module: :public do
@@ -35,13 +35,16 @@ Rails.application.routes.draw do
       resources :comments, only: [:create, :destroy]
       resource :favorites, only: [:create, :destroy]
     end
-    
+
     resources :users, only: [:show, :edit, :update] do
       resource :relationships, only: [:create, :destroy]
       get 'followings' => 'relationships#followings', as: 'followings'
       get 'followers' => 'relationships#followers', as: 'followers'
+      member do
+        get :favorites
+      end
     end
-    
+
     get 'users/unsubscribe' => 'users#unsubscribe', as: 'unsubscribe'
     patch 'users/withdraw' => "users#withdraw", as: 'withdraw'
     get "search" => "searches#search"
