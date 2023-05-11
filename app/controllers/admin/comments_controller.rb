440072c821f6be89e8725d58c5a45fdef6acdbc4
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-05-11 06:18:45 +0000

    update_管理者投稿一覧

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,9 +1,13 @@
 class Admin::CommentsController < ApplicationController
-  
+
   def update
+    @post = Post.find(params[:post_id])
+
   end
-  
+
   def destroy
+    Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+    redirect_to admin_post_path(params[:post_id])
   end
-  
+
 end
