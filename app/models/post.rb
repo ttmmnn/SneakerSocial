bb0a453b0b9952d289687b28519588035e8be630
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-05-18 07:16:53 +0000

    update_image

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,7 +14,8 @@ class Post < ApplicationRecord
       file_path = Rails.root.join('app/assets/images/noimage.png')
       image.attach(io: File.open(file_path), filename: 'default-image.png', content_type: 'image/png')
     end
-    image.variant(resize_to_limit: [width, height]).processed
+    # image.variant(resize_to_limit: [width, height]).processed
+    image.variant(resize: "#{width}x#{height}^", gravity: "center", crop: "#{width}x#{height}+0+0").processed
   end
 
   def favorited?(user)
