f378c433894420369f2aae172dd9885f24f7a020
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-06-13 11:50:44 +0000

    add_コメント機能非同期通信

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -5,17 +5,24 @@ class Public::CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
     @comment.user_id = current_user.id
     @comment.post_id = @post.id
-    @comment.save
-    redirect_to post_path(@post.id)
+    if @comment.save
+      @comments = @post.comments.page(params[:page]).per(5).reverse_order
+      render :post_comments
+    else
+      render 'posts/show'
+    end
   end
 
   def destroy
     Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
-    redirect_to post_path(params[:post_id])
+    # redirect_to post_path(params[:post_id])
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments.page(params[:page]).per(5).reverse_order
+    render :post_comments
   end
 
   private
-  
+
   def comment_params
     params.require(:comment).permit(:comment_content)
   end
